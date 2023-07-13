ecfb1ffa65121567b8e01982384c706f7dfa22cf
Nightmare-twilight <138739539+Nightmare-twilight@users.noreply.github.com>
2023-07-13 14:58:29 +0900

    文字入力
    文字を入力した

--- match３/match３/ヘッダー.h
+++ match３/match３/ヘッダー.h
@@ -24,5 +24,28 @@
 プロトタイプ宣言
 ***********************************/
 
-int WINAPI WnMain(_In_HINSTANCE hinstance,_In_opt_HINSTANCE hPrevInstance,
-	_In_LPSTR lpCMdLine,_Int nShowCMd)
+int WINAPI WnMain(_In_HINSTANCE hinstance, _In_opt_HINSTANCE hPrevInstance,
+	_In_LPSTR lpCMdLine, _Int nShowCMd)
+{
+
+
+	//ウィンドウタイトル設定
+	setmainWindowText("Match3Puzzle");
+	//ウィンドモード起動
+	ChangeWindowMode(TRUE);
+	//画面サイズの最大サイズ、カラービット数を設定
+	setGraphMode(SCREEN_WIDDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
+	//Dxライブラリ初期化処理
+	//エラーが発生したら、終了
+	if (DxLib_Init()ERROR)
+	{
+		return D_ERROR;
+	}
+//各機能の初期化処理
+	FreamControl_Initialize();
+	ZInput_Initialize();
+	//シーンマネージャー初期化処理
+	//エラーが発生したら、終了
+	if{}
+}
