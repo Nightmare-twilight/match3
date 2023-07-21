e1b102ee2452d460f70493a1bec8f8af9ca7cc9d
Nightmare-twilight <138739539+Nightmare-twilight@users.noreply.github.com>
2023-07-21 12:08:56 +0900

    ヘッダーファイルを作った
    ヘッダーファイルの追加

--- match３/match３/AppMain.cpp
+++ match３/match３/AppMain.cpp
@@ -6,6 +6,8 @@
 
 #include"DxLib.h"
 #include"FreamControl.h"
+#include"InputControl.h"
+#include"SceneManager.h"
 
 /*************
 *マクロ定義
@@ -17,8 +19,6 @@
 /**************************************
 *型定義
 ***************************************/
-
-
 
 /***************************************
 プロトタイプ宣言
@@ -52,12 +52,30 @@ int WINAPI WnMain(_In_ HINSTANCE hinstance, _In_opt_ HINSTANCE hPrevInstance,
 		return D_ERROR;
 	}
 	//描画先画面を裏にする
-	setDrawScreen(DX_SCREEN_BACK);
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	//文字サイズ設定
-	setFontSize(FONT_SIZE);
+	SetFontSize(FONT_SIZE);
 
 
 	//ゲームループ
-	while
+	while (ProcessMessage() != D_ERROR && Input_Escap() == FALSE)
+	{
+//入力制御機能更新処理
+		Input_Update();
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+		
+		//画面クリア
+		ClearDrawScreen();
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+		//フレームレート制御処理
+		FreamControl_Update();
+		//画面の内容を表画面に反映
+		ScreenFlip();
+	}
+	//Dxライブラリ使用の終了処理
+	DxLib_End();
+	return 0;
 }
